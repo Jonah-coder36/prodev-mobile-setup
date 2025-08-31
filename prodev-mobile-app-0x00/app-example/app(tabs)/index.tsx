44279dab47b49d0697b62e0030e68917e0cd783d
Yona <yonaakuma@gmail.com>
2025-08-31 22:18:28 +0300

    Update index.tsx

--- prodev-mobile-app-0x00/app-example/app(tabs)/index.tsx
+++ prodev-mobile-app-0x00/app-example/app(tabs)/index.tsx
@@ -1,3 +1,13 @@
-//index
-<Text> First App Created </Text>
-reset-project
+## 📄 app-example/app/(tabs)/index.tsx
+
+```tsx
+import { Text, View } from "react-native";
+
+export default function TabOneScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text> First App Created </Text>
+    </View>
+  );
+}
+
